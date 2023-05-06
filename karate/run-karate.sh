743bb35082d1b4ecb970135e5552bef76993128b
Matt <matt@pop-os.localdomain>
2023-05-06 14:29:19 -0400

    Enhance script and add some debug

--- karate/run-karate.sh
+++ karate/run-karate.sh
@@ -2,8 +2,10 @@
 
 if hash buildkite-agent 2>/dev/null; then
   xyz_api_url=$(buildkite-agent meta-data get "xyz_api_url")
+  echo "xyz_api_url is $xyz_api_url"
   java -jar ./karate-1.4.0.jar --env=buildkite ./message.feature
 else
+  echo "Running outside of buildkite"
   java -jar ./karate-1.4.0.jar ./message.feature
 fi
 
