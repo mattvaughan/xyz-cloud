bd77f01585dacb638f2d4ced0a43ca9e2d653ffb
Matt <matt@pop-os.localdomain>
2023-05-07 16:17:56 -0400

    Allow breakage

--- src/Xyz.UnitTests/MessageTest.cs
+++ src/Xyz.UnitTests/MessageTest.cs
@@ -10,7 +10,7 @@ public class MessageTest
     {
       var messageController = new MessageController();
       var result = messageController.Get();
-      Assert.Equal("Automate all the things!", result.Message);
+      Assert.Equal("sdfasdfasdfasdfasdf", result.Message);
       Assert.True(result.Timestamp > 0, "Timestamp should be set!");
     }
 }
