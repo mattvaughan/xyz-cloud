f991ee8780b418c7bb4576512d6439178626ea2f
Matt <matt@pop-os.localdomain>
2023-05-16 10:28:37 -0400

    More emphasis

--- src/Xyz.Api/Controllers/MessageController.cs
+++ src/Xyz.Api/Controllers/MessageController.cs
@@ -16,7 +16,7 @@ public class MessageController : ControllerBase
   {
     var message = new MessageModel
     {
-      Message = "Automate all the things!",
+      Message = "Automate all the things!!!",
       Timestamp = DateTimeOffset.UtcNow.ToUnixTimeSeconds()
     };
     return message;
